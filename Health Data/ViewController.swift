2cebb973a3ca35430d80ee27b2ff54c16307fb8f
謝清荃 <shane72037@hotmail.com>
2018-08-07 16:37:46 +0800

    Initial Commit

---
+++ Health Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health Data
+//
+//  Created by 謝清荃 on 2018/8/7.
+//  Copyright © 2018年 Shane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
